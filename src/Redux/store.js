efd1d115659af68595c640ad7c3060699a04d3aa
emonkhan500 <khdilo47@gmail.com>
2024-12-10 20:15:23 +0600

    form created

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './feature/CounterSlice'
+import logger from 'redux-logger'
 export const store= configureStore({
     reducer:{
         counter: counterReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
